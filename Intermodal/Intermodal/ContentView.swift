1fa88ccf28098be68a091b02f81dc510908e7c6d
Matthew LaBarca <matthewlabarca@Matthews-MacBook-Pro.local>
2024-10-26 13:29:49 -0400

    first commit

---
+++ Intermodal/Intermodal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Intermodal
+//
+//  Created by Matthew LaBarca on 10/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Intermodal/Intermodal/IntermodalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermodalApp.swift
+//  Intermodal
+//
+//  Created by Matthew LaBarca on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct IntermodalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
