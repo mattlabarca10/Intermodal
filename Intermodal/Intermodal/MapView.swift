f3fdd3a935db51fcfc85dd47a77a70f6c7d3bd71
Matthew LaBarca <matthewlabarca@Matthews-MacBook-Pro.local>
2024-10-26 13:48:15 -0400

    MapView format

--- Intermodal/Intermodal/MapView.swift
+++ Intermodal/Intermodal/MapView.swift
@@ -1,8 +1,28 @@
-//
-//  MapView.swift
-//  Intermodal
-//
-//  Created by Matthew LaBarca on 10/26/24.
-//
+// MapView.swift
+// Intermodal
+// Created by Matthew LaBarca on 10/26/24.
 
 import Foundation
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        VStack {
+            Text("Intermodal")
+            Map()
+            Text("Breakdown")
+                .padding(.bottom, 200)
+        }
+
+    }
+}
+
+#Preview {
+    MapView()
+}
+
+
+/*
+
+ */
