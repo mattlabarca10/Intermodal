28989a001f8a64654b817cee93333b55aa7cfd0b
Abhiram Thotkura <athotkura@gmail.com>
2024-10-26 21:48:41 -0400

    distance

--- Intermodal/Intermodal/ContentView.swift
+++ Intermodal/Intermodal/ContentView.swift
@@ -9,11 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack() {
-            Text("hello world")
+        WelcomeView()
         }
     }
-}
+
 #Preview {
     ContentView()
 }
